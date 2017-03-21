1cf31a866012243992a1941e946b6ab5dd02efa3
semitable <almak72@gmail.com>
2017-03-21 19:31:11 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='easing_functions',
+    version='1.0.0',
+    packages=['easing_functions'],
+    url='https://github.com/semitable/easing-functions',
+    license='GPL 3.0',
+    author='Filippos Christianos',
+    author_email='almak72@gmail.com',
+    description='A collection of the basic easing functions for python'
+)
